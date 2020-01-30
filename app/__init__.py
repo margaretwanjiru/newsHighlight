cf6d5e1be398f8d933fa273b96f550adfa150f3f
irungumargaret <sairmargaret44@gmail.com>
2020-01-30 14:18:57 +0300

    import the flask,bootsrap and config_options and register the blueprint

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config):
+	app = Flask(__name__)
+
+	#create the app configurations
+	app.config.from_object(config_options[config])
+
+	# Initializing flask extensions
+	bootstrap.init_app(app)
+
+
+	#registering the blueprint
+	from .main import main as main_blueprint
+	app.register_blueprint(main_blueprint)
+
+	#setting config
+	from .requests import configure_request
+	configure_request(app)
+
+	return app
